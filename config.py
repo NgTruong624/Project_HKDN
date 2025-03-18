9f77d90ee805f81aeb0d870c8ed8dc8ce414fd25
Tran Vo Ngoc Truong <119861415+Etouu624@users.noreply.github.com>
2025-03-18 08:59:50 +0700

    deploy

--- config.py
+++ config.py
@@ -6,6 +6,11 @@ load_dotenv()
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///epl_players.db'
+    if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     KAGGLE_USERNAME = os.environ.get('KAGGLE_USERNAME')
-    KAGGLE_KEY = os.environ.get('KAGGLE_KEY') 
+    KAGGLE_KEY = os.environ.get('KAGGLE_KEY')
+    
+    # CORS settings
+    CORS_HEADERS = 'Content-Type' 
